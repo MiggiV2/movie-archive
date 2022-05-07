ab612a1a745cd582e8fafcb3a1de2a9dc1e37fbe
MiggiV2 <miggi@familyhainz.de>
2022-05-07 21:14:03 +0200

    node 16 lts fix

--- gui/src/router/index.js
+++ gui/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/views/HomePage';
+import HomePage from '@/views/HomePageView';
 
 const routes = [{
         path: '/',
@@ -14,22 +14,22 @@ const routes = [{
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "auth" */ '@/views/Auth.vue')
+            import ( /* webpackChunkName: "auth" */ '@/views/AuthView.vue')
     }, {
         path: '/search',
         name: 'search',
         component: () =>
-            import ( /* webpackChunkName: "search" */ '@/views/Search.vue')
+            import ( /* webpackChunkName: "search" */ '@/views/SearchView.vue')
     }, {
         path: '/add',
         name: 'add',
         component: () =>
-            import ( /* webpackChunkName: "add" */ '@/views/Add.vue')
+            import ( /* webpackChunkName: "add" */ '@/views/AddView.vue')
     }, {
         path: '/audit-log',
         name: 'audit-log',
         component: () =>
-            import ( /* webpackChunkName: "search" */ '@/views/AuditLog.vue')
+            import ( /* webpackChunkName: "search" */ '@/views/AuditLogView.vue')
     }
 ]
 
