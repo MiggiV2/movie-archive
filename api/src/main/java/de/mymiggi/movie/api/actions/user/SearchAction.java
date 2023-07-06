10fdf582419e1e84d8c34163d1987f74ea2cde55
Miggi <miggi@noreply.familyhainz.de>
2023-07-06 09:40:20 +0200

    :fire: Removed SimpleSearchAction class

--- api/src/main/java/de/mymiggi/movie/api/actions/user/SearchAction.java
+++ api/src/main/java/de/mymiggi/movie/api/actions/user/SearchAction.java
@@ -5,8 +5,8 @@ import java.util.List;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import de.mymiggi.movie.api.actions.helper.SimpleSearchAction;
 import de.mymiggi.movie.api.entity.MessageStatus;
+import de.mymiggi.movie.api.entity.SearchWrapper;
 import de.mymiggi.movie.api.entity.ShortMessage;
 import de.mymiggi.movie.api.entity.db.MovieEntity;
 
@@ -19,9 +19,31 @@ public class SearchAction
 			ShortMessage message = new ShortMessage("You need a search query!", MessageStatus.ERROR);
 			return Response.status(Status.BAD_REQUEST).entity(message).build();
 		}
-		List<MovieEntity> result = new SimpleSearchAction().run(query);
+		List<MovieEntity> result = search(query);
 		return result.isEmpty()
 			? Response.noContent().build()
 			: Response.ok(result).build();
+	}
+
+	/**
+	 * <h3>After some extrem tests:</h3>
+	 * <p>
+	 * This stream method is ~5.85% - <0.9% slower [JVM dev mode]
+	 * </p>
+	 * <p>
+	 * BUT it's faster via <b>GralVM</b>! Up to 5.5% <b>FASTER</b>
+	 * </p>
+	 *
+	 * @return sorted result
+	 */
+	private List<MovieEntity> search(String query)
+	{
+		List<MovieEntity> allMovies = MovieEntity.listAll();
+		return allMovies.stream()
+			.map(m -> new SearchWrapper(m, query))
+			.sorted()
+			.filter(m -> m.getMatchesQuery() > 0)
+			.map(SearchWrapper::getMovie)
+			.toList();
 	}
 }
