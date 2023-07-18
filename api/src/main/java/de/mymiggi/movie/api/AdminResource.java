3e8ac0a7982e920d88c1ddc610b5a013f49001aa
Miggi <miggi@noreply.familyhainz.de>
2023-07-18 16:55:42 +0200

    :art: added Produces & Consumes

--- api/src/main/java/de/mymiggi/movie/api/AdminResource.java
+++ api/src/main/java/de/mymiggi/movie/api/AdminResource.java
@@ -4,12 +4,15 @@ import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import de.mymiggi.movie.api.actions.admin.AddMovieAction;
@@ -25,6 +28,8 @@ import io.quarkus.security.identity.SecurityIdentity;
 @Path("movie-archive/admin")
 @RolesAllowed("admin")
 @ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AdminResource
 {
 	@Inject
