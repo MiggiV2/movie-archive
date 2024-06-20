b5d44215e5f7d661daeaf6fcd5abf7ebc997a466
Miggi <noreply@mymiggi.de>
2024-06-20 19:18:06 +0200

    :fire: (api) Unused import

--- api/src/main/java/de/mymiggi/movie/api/entity/db/MovieEntity.java
+++ api/src/main/java/de/mymiggi/movie/api/entity/db/MovieEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.SequenceGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
