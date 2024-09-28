d1441d025c1a813587a3f1656904fb0da93066ef
Miggi <noreply@mymiggi.de>
2024-09-28 17:05:26 +0200

    :wrench: (api): Better config

--- api/src/main/java/de/mymiggi/movie/api/entity/config/DefaultPage.java
+++ api/src/main/java/de/mymiggi/movie/api/entity/config/DefaultPage.java
@@ -2,7 +2,7 @@ package de.mymiggi.movie.api.entity.config;
 
 import io.smallrye.config.ConfigMapping;
 
-@ConfigMapping(prefix = "default-page")
+@ConfigMapping(prefix = "de.mymiggi.movie.default-page")
 public interface DefaultPage
 {
 	int Size();
--- api/src/main/java/de/mymiggi/movie/api/entity/config/OAuthConfig.java
+++ api/src/main/java/de/mymiggi/movie/api/entity/config/OAuthConfig.java
@@ -2,7 +2,7 @@ package de.mymiggi.movie.api.entity.config;
 
 import io.smallrye.config.ConfigMapping;
 
-@ConfigMapping(prefix = "oauth2")
+@ConfigMapping(prefix = "de.mymiggi.movie.oauth2")
 public interface OAuthConfig
 {
 	String RedirectURL();
